06f2be5629fdd67e12fd811f4004603167ee6fe6
francis selorm kponyo <pojoba01@gmail.com>
2023-04-28 14:33:17 -0700

    Update settings.py

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'django_media_fixtures',  # include this to work, bellow django.contrib.staticfiles
-
     'fooapp',
 )
 
