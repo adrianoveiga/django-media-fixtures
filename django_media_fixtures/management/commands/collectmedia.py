36e73f0f48f817698b061016a62bf91ace731187
francis selorm kponyo <pojoba01@gmail.com>
2023-04-29 12:33:06 -0700

    Update collectmedia.py

--- django_media_fixtures/management/commands/collectmedia.py
+++ django_media_fixtures/management/commands/collectmedia.py
@@ -221,9 +221,9 @@ class Command(BaseCommand):
             fpath = os.path.join(path, f)
             if self.dry_run:
                 self.log("Pretending to delete '%s'" %
-                         smart_text(fpath), level=1)
+                         force_str(fpath), level=1)
             else:
-                self.log("Deleting '%s'" % smart_text(fpath), level=1)
+                self.log("Deleting '%s'" % force_str(fpath), level=1)
                 self.storage.delete(fpath)
         for d in dirs:
             self.clear_dir(os.path.join(path, d))
